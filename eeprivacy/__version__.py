4cc74a753c1e32590f613aa61669471f572a7625
Marc-Antoine Paré <marc@recurve.com>
2020-09-29 11:30:42 -0700

    First cut at pypi set up

---
+++ eeprivacy/__version__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
+__title__ = "eeprivacy"
+__description__ = "Energy Differential Privacy"
+__url__ = "http://github.com/openeemeter/eeprivacy"
+__version__ = "0.0.1"
+__author__ = "Marc Paré"
+__author_email__ = "marc@recurve.com"
--- setup.py
+++ setup.py
@@ -1,13 +1,77 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This setup.py inspired by https://github.com/kennethreitz/setup.py
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
+
+NAME = "eeprivacy"
+REQUIRED = ["click", "pandas", "scipy"]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, "__version__.py")) as f:
+    exec(f.read(), about)
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
 
 setup(
-    name='eeprivacy',
-    version='0.1',
-    description='',
-    url='',
-    author='',
-    author_email='',
-    license='',
-    packages=['eeprivacy'],
-    zip_safe=False,
+    name=NAME,
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=long_description,
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    classifiers=[],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
 )
