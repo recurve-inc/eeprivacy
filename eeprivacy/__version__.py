5228bcad743c64f49cc90b19774847e7e6bb4859
Marc-Antoine Paré <marc@recurve.com>
2020-11-10 11:19:11 -0800

    Release update

--- eeprivacy/__version__.py
+++ eeprivacy/__version__.py
@@ -5,6 +5,6 @@
 __title__ = "eeprivacy"
 __description__ = "Energy Differential Privacy"
 __url__ = "http://github.com/openeemeter/eeprivacy"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Marc Paré"
 __author_email__ = "marc@recurve.com"
